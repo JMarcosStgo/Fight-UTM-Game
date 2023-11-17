70240a97134771ca77b756f525c7d2e8ba3df5b8
Frantchet <franchet.2000@gmail.com>
2023-11-16 22:54:24 -0600

    bugFixesAndEnemyDifficulty

--- Assets/Scripts/Player2Acciones.cs
+++ Assets/Scripts/Player2Acciones.cs
@@ -8,6 +8,9 @@ public class Player2Acciones : MonoBehaviour
     public float rotacion = 100.0f;
     private Animator anim;
     public float x, y, mandoX, mandoY;
+    private bool animacion = false;
+    private int aux = 0;
+    private bool fin = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,52 +20,73 @@ public class Player2Acciones : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxis("Horizontal2");
-        y = Input.GetAxis("Vertical2");
-
-        mandoX = Input.GetAxis("6th axis2");
-        mandoY = Input.GetAxis("7th axis2");
-
-        transform.Rotate(0, y * Time.deltaTime * rotacion, 0);
-        transform.Translate(0, 0, x * Time.deltaTime * velocidad);
-
-        transform.Rotate(0, mandoY * Time.deltaTime * rotacion, 0);
-        transform.Translate(0, 0, mandoX * Time.deltaTime * velocidad * -1);
-
-        //transform.Translate(-y * Time.deltaTime * velocidad, 0, 0);
-        anim.SetFloat("X", x);
-        //anim.SetFloat("X", mandoX);
-
-        if (Input.GetKeyDown(KeyCode.J) || Input.GetKey(KeyCode.Joystick2Button2))
+        if (!fin)
         {
-            anim.SetTrigger("punch1");
-        }
+            if (!animacion)
+            {
+                aux = 0;
+                x = Input.GetAxis("Horizontal2");
+                y = Input.GetAxis("Vertical2");
 
-        if (Input.GetKeyDown(KeyCode.I) || Input.GetKey(KeyCode.Joystick2Button3))
-        {
-            anim.SetTrigger("punch2");
-        }
+                mandoX = Input.GetAxis("6th axis2");
+                mandoY = Input.GetAxis("7th axis2");
 
-        if (Input.GetKeyDown(KeyCode.K) || Input.GetKey(KeyCode.Joystick2Button0))
-        {
-            anim.SetTrigger("kick1");
-        }
+                transform.Rotate(0, y * Time.deltaTime * rotacion, 0);
+                transform.Translate(0, 0, x * Time.deltaTime * velocidad);
 
-        if (Input.GetKeyDown(KeyCode.L) || Input.GetKey(KeyCode.Joystick2Button1))
-        {
-            anim.SetTrigger("kick2");
-        }
+                transform.Rotate(0, mandoY * Time.deltaTime * rotacion, 0);
+                transform.Translate(0, 0, mandoX * Time.deltaTime * velocidad * -1);
 
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            anim.SetTrigger("victory");
-            return;
-        }
+                //transform.Translate(-y * Time.deltaTime * velocidad, 0, 0);
+                anim.SetFloat("X", x);
+                //anim.SetFloat("X", mandoX);
 
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            anim.SetTrigger("lose");
-            return;
+                if (Input.GetKeyDown(KeyCode.J) || Input.GetKey(KeyCode.Joystick2Button2))
+                {
+                    anim.SetTrigger("punch1");
+                    animacion = true;
+                }
+
+                if (Input.GetKeyDown(KeyCode.I) || Input.GetKey(KeyCode.Joystick2Button3))
+                {
+                    anim.SetTrigger("punch2");
+                    animacion = true;
+                }
+
+                if (Input.GetKeyDown(KeyCode.K) || Input.GetKey(KeyCode.Joystick2Button0))
+                {
+                    anim.SetTrigger("kick1");
+                    animacion = true;
+                }
+
+                if (Input.GetKeyDown(KeyCode.L) || Input.GetKey(KeyCode.Joystick2Button1))
+                {
+                    anim.SetTrigger("kick2");
+                    animacion = true;
+                }
+
+                if (Input.GetKeyDown(KeyCode.H))
+                {
+                    anim.SetTrigger("victory");
+                    animacion = true;
+                    fin = true;
+                }
+
+                if (Input.GetKeyDown(KeyCode.Y))
+                {
+                    anim.SetTrigger("lose");
+                    animacion = true;
+                    fin = true;
+                }
+            }
+            else
+            {
+                aux++;
+                if (aux == 60)
+                {
+                    animacion = false;
+                }
+            }
         }
     }
 }
